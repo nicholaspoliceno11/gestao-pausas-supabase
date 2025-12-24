80b68a1dc66d6dbff3023a26fdf43fddc604cca6
nicholaspoliceno11 <nicholas.maceio.al@gmail.com>
2025-12-24 16:04:27 -0300

    Update main_supabase.py

--- main_supabase.py
+++ main_supabase.py
@@ -13,7 +13,7 @@ from email.mime.multipart import MIMEMultipart
 GMAIL_USER = "gestao.queropassagem@gmail.com"
 GMAIL_PASSWORD = "pakiujauoxbmihyy"
 DISCORD_WEBHOOK_EQUIPE = "https://discord.com/api/webhooks/1452314030357348353/-ty01Mp6tabaM4U9eICtKHJiitsNUoEa9CFs04ivKmvg2FjEBRQ8CSjPJtSD91ZkrvUi"
-DISCORD_WEBHOOK_SAC_QP = "https://discord.com/api/webhooks/1452088104616722475/mIVeSKVD0mtLErmlTt5QqnQvYpDBEw7TpH7CdZB0A0H1Ms5iFWZqZdGmcRY78EpsJ_pI"
+DISCORD_WEBHOOK_SAC_QP = "https://discord.com/api/webhooks/1453463270517244091/x1KdnNbW8oR-yslJLb5zL5hdLHZXzHLq-I5RwTb67O16d8ccWh69FI63J2i1KJlYmQq4"
 SUPABASE_URL = "https://gzozqxrlgdzjrqfvdxzw.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6b3pxeHJsZ2R6anJxZnZkeHp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0OTg1MjIsImV4cCI6MjA4MjA3NDUyMn0.dLEjBPESUz5KnVwxqEMaMxoy65gsLqG2QdjK2xFTUhU"
 
